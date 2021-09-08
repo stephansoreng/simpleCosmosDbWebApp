b0533d7fc90461e205ef628a9f6346089fdf0f44
Stephan Kristiansen <stephan.soreng@gmail.com>
2021-09-08 15:14:42 +0200

    initial commit

---
+++ webappdemo2/Controllers/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using webappdemo2.Services;
+
+namespace webappdemo2.Controllers
+{
+    public class ItemController : Controller
+    {
+
+        private readonly ICosmosDbService _cosmosDbService;
+        public ItemController(ICosmosDbService cosmosDbService)
+        {
+            _cosmosDbService = cosmosDbService;
+        }
+        [ActionName("Index")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _cosmosDbService.GetItemsAsync("SELECT * FROM c"));
+        }
+    }
+}
---
+++ webappdemo2/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webappdemo2.Models
+{
+    using Newtonsoft.Json;
+    public class Item
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+    }
+}
